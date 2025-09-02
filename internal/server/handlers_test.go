c020ddafc3e04116c3ba06087f040ec5c52e5481
Denis Safiullin <fckninja@gmail.com>
2025-09-02 14:30:08 +0000

    Add unit test for PingHandler to verify response status and structure

---
+++ internal/server/handlers_test.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestPingHandler(t *testing.T) {
+	http.HandleFunc("GET /ping", PingHandler)
+	respRecorder := httptest.NewRecorder()
+	req, err := http.NewRequest(http.MethodGet, "/ping", nil)
+	if err != nil {
+		t.Fatalf("couldn't create a GET request to /ping endpoint, error: %v+", err)
+	}
+	http.DefaultServeMux.ServeHTTP(respRecorder, req)
+
+	if respRecorder.Code != http.StatusOK {
+		t.Error("response http code is not 200")
+	}
+
+	var pingResponse PingResponse
+	if err := json.Unmarshal(respRecorder.Body.Bytes(), &pingResponse); err != nil {
+		t.Errorf("couldn't unmarshal response body to expected ping response, error: %v+", err)
+	}
+
+	if expected, got := 1, pingResponse.Pong; expected != got {
+		t.Errorf("expected: %d, got: %d", expected, got)
+	}
+}
